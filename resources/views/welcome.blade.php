faceb23a3b39993e22a39c155388683a22505fe2
Murat <Murat>
2016-11-01 15:15:09 +0200

    heroku test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Murat's Laravel
                 </div>
 
                 <div class="links">
